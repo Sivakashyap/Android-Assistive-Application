a7b06b3f14697b4265958a198c9790a14303a081
Siva Kashyap Ch <130291262+Sivakashyap@users.noreply.github.com>
2024-05-28 23:18:21 +0530

    Add files via upload

---
+++ src/main/java/com/programminghut/realtime_object/MainActivity.kt
@@ -0,0 +1,188 @@
+package com.programminghut.realtime_object
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.speech.tts.TextToSpeech
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.programminghut.realtime_object.ml.SsdMobilenetV11Metadata1
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    lateinit var labels: List<String>
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED
+    )
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var model: SsdMobilenetV11Metadata1
+    lateinit var textToSpeech: TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getPermission()
+
+        labels = FileUtil.loadLabels(this, "labels.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = SsdMobilenetV11Metadata1.newInstance(this)
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        imageView = findViewById(R.id.imageView)
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {}
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+                val outputs = model.process(image)
+                val locations = outputs.locationsAsTensorBuffer.floatArray
+                val classes = outputs.classesAsTensorBuffer.floatArray
+                val scores = outputs.scoresAsTensorBuffer.floatArray
+
+                val mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+
+                val h = mutable.height
+                val w = mutable.width
+                paint.textSize = h / 15f
+                paint.strokeWidth = h / 85f
+                scores.forEachIndexed { index, fl ->
+                    if (fl > 0.5) {
+                        paint.color = colors[index]
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(
+                            RectF(
+                                locations[index * 4 + 1] * w,
+                                locations[index * 4] * h,
+                                locations[index * 4 + 3] * w,
+                                locations[index * 4 + 2] * h
+                            ),
+                            paint
+                        )
+                        paint.style = Paint.Style.FILL
+                        val label = labels[classes[index].toInt()]
+                        val text = "$label ${fl.toString()}"
+                        canvas.drawText(text, locations[index * 4 + 1] * w, locations[index * 4] * h, paint)
+
+                        // Speak the detected object
+                        speakOut(label)
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
+            }
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        // Initialize Text-to-Speech
+        textToSpeech = TextToSpeech(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+        // Shutdown Text-to-Speech
+        textToSpeech.stop()
+        textToSpeech.shutdown()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun openCamera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
+                val surfaceTexture = textureView.surfaceTexture
+                val surface = Surface(surfaceTexture)
+
+                val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {}
+                }, handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {}
+
+            override fun onError(p0: CameraDevice, p1: Int) {}
+        }, handler)
+    }
+
+    fun getPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            getPermission()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Set preferred language for Text-to-Speech
+            val result = textToSpeech.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Language data is missing or the language is not supported
+                // Handle this as needed
+            }
+        } else {
+            // Text-to-Speech initialization failed
+            // Handle this as needed
+        }
+    }
+
+    private fun speakOut(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+}
